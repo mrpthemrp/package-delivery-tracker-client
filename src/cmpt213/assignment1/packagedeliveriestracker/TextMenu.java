291360e0f4c094193dca5d0f6ab48a89b6ae6607
Deborah Wang <deborah.0214@hotmail.com>
2022-05-20 14:39:50 -0700

    Fill in basics for TextMenu class

--- src/cmpt213/assignment1/packagedeliveriestracker/TextMenu.java
+++ src/cmpt213/assignment1/packagedeliveriestracker/TextMenu.java
@@ -1,4 +1,26 @@
 package cmpt213.assignment1.packagedeliveriestracker;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 public class TextMenu {
+    private String menuTitle;
+    private ArrayList<String> menuOptions;
+
+    public TextMenu(String menuTitle){
+        this.menuTitle = menuTitle;
+
+        //initialize ArrayList
+        menuOptions = new ArrayList<>();
+    }
+
+    public String getMenuTitle() {
+        return menuTitle;
+    }
+
+    public void displayMenu(){
+        System.out.println("displayMenu works!");
+    }
+
+    //read input method to implement
 }
