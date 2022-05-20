1ffb40c268599fa916ec184c7c46b90880d52247
Deborah Wang <deborah.0214@hotmail.com>
2022-05-20 15:14:52 -0700

    TextMenu constructor half-finish

--- src/cmpt213/assignment1/packagedeliveriestracker/PackageDeliveryTracker.java
+++ src/cmpt213/assignment1/packagedeliveriestracker/PackageDeliveryTracker.java
@@ -1,7 +1,23 @@
 package cmpt213.assignment1.packagedeliveriestracker;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PackageDeliveryTracker {
+    private TextMenu menu;
+    private ArrayList<Package> listOfPackages;
+
+    public PackageDeliveryTracker () {
+        menu = new TextMenu("Package Menu");
+        listOfPackages = new ArrayList<>();
+    }
+
     public static void main(String[] args) {
-        System.out.println("First run");
+        PackageDeliveryTracker self = new PackageDeliveryTracker();
+
+        self.menu.getInput();
+
+        //System.out.println("Main works");
+        //use a switch case for the menu!
     }
 }
--- src/cmpt213/assignment1/packagedeliveriestracker/TextMenu.java
+++ src/cmpt213/assignment1/packagedeliveriestracker/TextMenu.java
@@ -2,16 +2,26 @@ package cmpt213.assignment1.packagedeliveriestracker;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class TextMenu {
     private String menuTitle;
     private ArrayList<String> menuOptions;
+    private Scanner input = new Scanner(System.in);
 
     public TextMenu(String menuTitle){
         this.menuTitle = menuTitle;
 
         //initialize ArrayList
         menuOptions = new ArrayList<>();
+        menuOptions.add("List All Packages");
+        menuOptions.add("Add A Package");
+        menuOptions.add("Remove A Package");
+        menuOptions.add("List Overdue Packages");
+        menuOptions.add("List Upcoming Packages");
+        menuOptions.add("Mark Package As Delivered");
+        menuOptions.add("Exit");
     }
 
     public String getMenuTitle() {
@@ -19,8 +29,28 @@ public class TextMenu {
     }
 
     public void displayMenu(){
-        System.out.println("displayMenu works!");
+        String hashTags ="####";// to account for spaces
+        for(int i =0;i<menuOptions.size();i++){
+            hashTags+="#";
+        }
+        System.out.println(hashTags+"\n# "+menuTitle+" #\n"+hashTags);
+        System.out.println("Today is: DATE (to be implemented)");
     }
 
-    //read input method to implement
+    public int getInput() throws InputMismatchException{
+        boolean correctInput = false;
+        int userInput = 0;
+
+        while(!correctInput){
+            try{
+                userInput = input.nextInt();
+                correctInput = true;
+            } catch (InputMismatchException e){
+                System.out.println("Incorrect input. Please try again.");
+                input.next();
+            }
+        }
+
+        return userInput;
+    }
 }
