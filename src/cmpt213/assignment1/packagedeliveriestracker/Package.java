eb01937b2de9d4b9776a5aa84ae405c073bf2443
Deborah Wang <deborah.0214@hotmail.com>
2022-06-03 10:25:53 -0700

    Finish JavaDoc for Package class

--- src/cmpt213/assignment1/packagedeliveriestracker/Package.java
+++ src/cmpt213/assignment1/packagedeliveriestracker/Package.java
@@ -4,43 +4,28 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * A class that is modelled after a package.
+ * Package class that is modelled after a package; stores
+ * name, notes, price, weight, date, and delivery status.
+ *
  * @author Deborah Wang
  */
-public class Package  {
+public class Package {
     private final String name;
     private final String notes;
     private final double price;
     private final double weight;
-    private boolean isDelivered;
     private final LocalDateTime expectedDeliveryDate;
+    private boolean isDelivered;
 
     /**
-     * Converts object's fields into String for printing.
-     * @return A string of the object's fields, ready for print.
-     */
-    @Override
-    public String toString() {
-        String deliveryStatus = "Not delivered.";
-        if (this.isDelivered) {
-            deliveryStatus = "Delivered.";
-        }
-
-        return ("Package Name: " + this.name + "\n" +
-                "Notes: " + this.notes + "\n" +
-                "Price (CAD): $" + this.price + "\n" +
-                "Weight (kg): " + this.weight + "kg\n" +
-                "Expected Delivery Date: " + this.expectedDeliveryDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm a")) + "\n" +
-                "Delivery Status: " + deliveryStatus);
-    }
-
-    /**
-     * Constructor for class, creates an object modelled as a package.
-     * @param name The name of the package, cannot be empty.
-     * @param notes Any notes for the package, can be empty.
-     * @param price The price (in CAD) of the package, cannot be negative.
+     * Constructor for class, creates an object modelled as a package
+     * with its respective fields.
+     *
+     * @param name   The name of the package, cannot be empty.
+     * @param notes  Any notes for the package, can be empty.
+     * @param price  The price (in CAD) of the package, cannot be negative.
      * @param weight The weight (in kg) of the package, cannot be negative.
-     * @param date The date of the package (Year, Month, Day, Hour, Minute), in 24-hour format.
+     * @param date   The date of the package (Year, Month, Day, Hour, Minute), in 24-hour format.
      */
     public Package(String name, String notes, double price,
                    double weight, LocalDateTime date) {
@@ -53,35 +38,64 @@ public class Package  {
         this.expectedDeliveryDate = date;
     }
 
-    //GETTERS, SETTER
+    /**
+     * Converts object's fields into a String that
+     * conveys what the fields are (units are included).
+     *
+     * @return A string of the object's fields, ready for print.
+     */
+    @Override
+    public String toString() {
+
+        String deliveryStatus = "Not delivered.";
+
+        if (this.isDelivered) {
+            deliveryStatus = "Delivered.";
+        }
+
+        return ("Package Name: " + this.name + "\n" +
+                "Notes: " + this.notes + "\n" +
+                "Price (CAD): $" + this.price + "\n" +
+                "Weight (kg): " + this.weight + "kg\n" +
+                "Expected Delivery Date: " +
+                this.expectedDeliveryDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm a")) +
+                "\nDelivery Status: " + deliveryStatus);
+    }
 
     /**
-     * Changes a package's delivery status.
-     * @param newStatus The new status of the package.
+     * Gets the package's delivery status;
+     * returns false if not delivered, true if delivered.
+     *
+     * @return A boolean value (true or false);
+     */
+    public boolean getDeliveryStatus() {
+        return this.isDelivered;
+    }
+
+    /**
+     * Changes a package's delivery status;
+     * either true or false.
+     *
+     * @param newStatus The new status of the package, bool value.
      */
     public void setDeliveryStatus(boolean newStatus) {
         this.isDelivered = newStatus;
     }
 
     /**
-     * Gets the boolean value of the package's delivery status.
-     * @return A boolean value.
-     */
-    public boolean getDeliveryStatus() {
-        return isDelivered;
-    }
-
-    /**
-     * Gets the delivery date of the package.
-     * @return A LocalDateTime object.
+     * Gets the delivery date of the package
+     * as a LocalDateTime object.
+     *
+     * @return A LocalDateTime object with the date information.
      */
     public LocalDateTime getExpectedDeliveryDate() {
-        return expectedDeliveryDate;
+        return this.expectedDeliveryDate;
     }
 
     /**
      * Gets the name of the package.
-     * @return A string that holds the name.
+     *
+     * @return A String that holds the name.
      */
     public String getName() {
         return this.name;
