277fc035e8cb32369eecd368bc7bf0cfa617c983
Deborah Wang <deborah.0214@hotmail.com>
2022-06-02 19:06:14 -0700

    Write JavaDoc for Package class

--- src/cmpt213/assignment1/packagedeliveriestracker/Package.java
+++ src/cmpt213/assignment1/packagedeliveriestracker/Package.java
@@ -3,6 +3,10 @@ package cmpt213.assignment1.packagedeliveriestracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A class that is modelled after a package.
+ * @author Deborah Wang
+ */
 public class Package  {
     private final String name;
     private final String notes;
@@ -11,6 +15,10 @@ public class Package  {
     private boolean isDelivered;
     private final LocalDateTime expectedDeliveryDate;
 
+    /**
+     * Converts object's fields into String for printing.
+     * @return A string of the object's fields, ready for print.
+     */
     @Override
     public String toString() {
         String deliveryStatus = "Not delivered.";
@@ -26,6 +34,14 @@ public class Package  {
                 "Delivery Status: " + deliveryStatus);
     }
 
+    /**
+     * Constructor for class, creates an object modelled as a package.
+     * @param name The name of the package, cannot be empty.
+     * @param notes Any notes for the package, can be empty.
+     * @param price The price (in CAD) of the package, cannot be negative.
+     * @param weight The weight (in kg) of the package, cannot be negative.
+     * @param date The date of the package (Year, Month, Day, Hour, Minute), in 24-hour format.
+     */
     public Package(String name, String notes, double price,
                    double weight, LocalDateTime date) {
         this.isDelivered = false;
@@ -38,19 +54,35 @@ public class Package  {
     }
 
     //GETTERS, SETTER
+
+    /**
+     * Changes a package's delivery status.
+     * @param newStatus The new status of the package.
+     */
     public void setDeliveryStatus(boolean newStatus) {
         this.isDelivered = newStatus;
     }
 
+    /**
+     * Gets the boolean value of the package's delivery status.
+     * @return A boolean value.
+     */
     public boolean getDeliveryStatus() {
         return isDelivered;
     }
 
+    /**
+     * Gets the delivery date of the package.
+     * @return A LocalDateTime object.
+     */
     public LocalDateTime getExpectedDeliveryDate() {
         return expectedDeliveryDate;
     }
 
-
+    /**
+     * Gets the name of the package.
+     * @return A string that holds the name.
+     */
     public String getName() {
         return this.name;
     }
