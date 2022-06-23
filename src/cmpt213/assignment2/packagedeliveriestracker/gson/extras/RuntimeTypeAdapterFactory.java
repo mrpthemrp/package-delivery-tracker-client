278d5d21c2212b4cd578eb75f58daff5070da1d5
Deborah Wang <deborah.0214@hotmail.com>
2022-06-22 20:40:57 -0700

    Add license in RuntimeTypeAdapterFactory class

--- src/cmpt213/assignment2/packagedeliveriestracker/gson/extras/RuntimeTypeAdapterFactory.java
+++ src/cmpt213/assignment2/packagedeliveriestracker/gson/extras/RuntimeTypeAdapterFactory.java
@@ -2,6 +2,22 @@ package cmpt213.assignment2.packagedeliveriestracker.gson.extras;
 /*
 * THIS CLASS IS COPIED FROM THE FOLLOWING:
 * https://github.com/google/gson/blob/master/extras/src/main/java/com/google/gson/typeadapters/RuntimeTypeAdapterFactory.java
+*
+*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
 * */
 import java.io.IOException;
 import java.util.LinkedHashMap;
