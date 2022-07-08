f725b1b73c09b6eae12759fccbf5b8a2be2aa107
Deborah Wang <deborah.0214@hotmail.com>
2022-07-08 14:01:11 -0700

    JFrame template set up in MainApp

--- src/cmpt213/assignment3/packagedeliveriestracker/PackageDeliveryGUI.java
+++ src/cmpt213/assignment3/packagedeliveriestracker/PackageDeliveryGUI.java
@@ -1,13 +1,35 @@
 package cmpt213.assignment3.packagedeliveriestracker;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class PackageDeliveryGUI implements ActionListener, ItemListener {
+    //Components
+    private JButton startBtn;
 
-    public PackageDeliveryGUI(){
+
+    //Containers
+    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private double xSize = (screenSize.getWidth() / 2);
+    private double ySize = (screenSize.getHeight() / 2);
+    private JFrame appFrame;
+
+    public PackageDeliveryGUI() {
+        //set up frame
+        appFrame = new JFrame("Package Delivery Tracker");//change string input later
+        appFrame.setSize((int) xSize, (int) ySize);
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //set up components
+        startBtn = new JButton("Click to start");//change inner later
+        appFrame.add(startBtn);
+
+        //complete frame setup
+        appFrame.setVisible(true);
 
     }
 
