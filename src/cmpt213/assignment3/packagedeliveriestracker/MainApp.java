f035793ca170d44ff4e2e1c77d0ae5df2d95851c
Deborah Wang <deborah.0214@hotmail.com>
2022-07-08 13:50:25 -0700

    Add PackageDeliveryGUI class and Action/Item Listeners

--- src/cmpt213/assignment3/packagedeliveriestracker/MainApp.java
+++ src/cmpt213/assignment3/packagedeliveriestracker/MainApp.java
@@ -1,4 +1,9 @@
 package cmpt213.assignment3.packagedeliveriestracker;
 
+import javax.swing.*;
+
 public class MainApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new PackageDeliveryGUI());
+    }
 }
---
+++ src/cmpt213/assignment3/packagedeliveriestracker/PackageDeliveryGUI.java
@@ -0,0 +1,23 @@
+package cmpt213.assignment3.packagedeliveriestracker;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class PackageDeliveryGUI implements ActionListener, ItemListener {
+
+    public PackageDeliveryGUI(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+}
