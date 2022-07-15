5b73ff0dd11f9ee9ad1eeed9f4d4e1e590b41f02
Deborah Wang <deborah.0214@hotmail.com>
2022-07-15 16:17:31 -0700

    Custom fonts applied

--- src/cmpt213/assignment3/packagedeliveriestracker/view/util/Util.java
+++ src/cmpt213/assignment3/packagedeliveriestracker/view/util/Util.java
@@ -1,33 +1,83 @@
 package cmpt213.assignment3.packagedeliveriestracker.view.util;
 
-import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
+//font resource: https://stackoverflow.com/questions/5652344/how-can-i-use-a-custom-font-in-java
 public final class Util {
+
+    public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public static double screenWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    public static double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    public static Color darkTeal = new Color(0, 109, 119);
+    public static Color midTeal = new Color(131, 197, 190);
+    public static Color lightTeal = new Color(237, 246, 249);
+    public static Color lightBrown = new Color(255, 221, 210);
+    public static Color darkBrown = new Color(226, 149, 120);
+    public static DateTimeFormatter clockFormat = DateTimeFormatter.ofPattern("hh:mm:ss a");
+    public static DateTimeFormatter currentDayFormat = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+    public static Font clockFont;
+    public static Font mainScreenDateFont;
+    public static Font btnTextFont;
+    public static Font titleFont;
+    public static Font subTitleFont;
+    public static Font bodyFont = new Font(Font.SANS_SERIF, Font.PLAIN, (int) (10 * (Util.screenSize.getWidth() * 0.001)));
+
+    static {
+        try {
+            clockFont = createCustomFont("Roboto-Bold.ttf", Font.BOLD, (int) (20 * (Util.screenSize.getWidth() * 0.0018)));
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        try {
+            mainScreenDateFont = createCustomFont("Roboto-Medium.ttf", Font.PLAIN, (int) (20 * (Util.screenSize.getWidth() * 0.0008)));
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        try {
+            btnTextFont = createCustomFont("Roboto-Bold.ttf", Font.BOLD, (int) (14 * (Util.screenSize.getWidth() * 0.0013)));
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        try {
+            subTitleFont = createCustomFont("Roboto-Medium.ttf", Font.PLAIN, (int) (12 * (Util.screenSize.getWidth() * 0.001)));
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        try {
+            titleFont = createCustomFont("Roboto-Bold.ttf", Font.BOLD, (int) (18 * (Util.screenSize.getWidth() * 0.001)));
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Font createCustomFont(String filename, int type, int fontSize) throws IOException, FontFormatException {
+        String fs = File.separator;
+        String[] pathNames = {"src", "cmpt213", "assignment3", "packagedeliveriestracker", "view", "font"};
+        String path = String.join(fs, pathNames);
+        Font newFont;
+
+        newFont = Font.createFont(Font.TRUETYPE_FONT, new File(path + fs + filename)).deriveFont(type, (float) fontSize);
+        GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(newFont);
+
+        return newFont;
+    }
 
     public enum SCREEN_STATE {
         START, MAIN, ADD_PACKAGE, UPCOMING, OVERDUE
     }
-
-    public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-    public static double screenWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-    public static double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-
-    public static Color darkTeal = new Color(0,109,119);
-    public static Color midTeal = new Color(131,197,190);
-    public static Color lightTeal = new Color(237,246,249);
-    public static Color lightBrown = new Color(255,221,210);
-    public static Color darkBrown = new Color(226,149,120);
-    public static DateTimeFormatter clockFormat = DateTimeFormatter.ofPattern("hh:mm:ss a");
-    public static DateTimeFormatter currentDayFormat = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
-
-    public static Font clockFont = new Font(Font.SANS_SERIF, Font.BOLD, (int) (20 * (Util.screenSize.getWidth() * 0.0018)));
-    public static Font mainScreenDateFont = new Font(Font.SANS_SERIF, Font.BOLD, (int) (20 * (Util.screenSize.getWidth() * 0.0008)));
-    public static Font btnTextFont = new Font(Font.SANS_SERIF, Font.BOLD, (int) (14 * (Util.screenSize.getWidth() * 0.0013)));
-
-    public static Font titleFont = new Font(Font.SANS_SERIF, Font.BOLD, (int) (18 * (Util.screenSize.getWidth() * 0.001)));
-    public static Font subTitleFont = new Font(Font.SANS_SERIF, Font.PLAIN, (int) (12 * (Util.screenSize.getWidth() * 0.001)));
-    public static Font bodyFont = new Font(Font.SANS_SERIF, Font.PLAIN, (int) (10 * (Util.screenSize.getWidth() * 0.001)));
 }
