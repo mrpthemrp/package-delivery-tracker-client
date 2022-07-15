833e28ac7183711ee563df836d9db5b07b15095d
Deborah Wang <deborah.0214@hotmail.com>
2022-07-15 15:18:16 -0700

    Finish Round button class implementation

--- src/cmpt213/assignment3/packagedeliveriestracker/view/util/RoundButton.java
+++ src/cmpt213/assignment3/packagedeliveriestracker/view/util/RoundButton.java
@@ -6,12 +6,11 @@ import java.awt.event.ActionListener;
 
 // reference: https://www.javacodex.com/More-Examples/2/14
 public class RoundButton extends JButton {
-    private Dimension buttonDimension;
-    private String label, btnText;
-    private Color btnColor, btnColorDark;
+    private final Color btnColor;
+    private final Color btnColorDark;
+    private String btnText;
 
     public RoundButton(String btnText, String label, ActionListener al, Color btnColor, Color btnColorDark) {
-        this.label = label;
         this.btnText = btnText;
         this.btnColor = btnColor;
         this.btnColorDark = btnColorDark;
@@ -27,7 +26,7 @@ public class RoundButton extends JButton {
     }
 
     private void setUpDimensions(Graphics g) {
-        buttonDimension = new Dimension((int) (g.getFontMetrics().stringWidth(this.btnText) * 1.25),
+        Dimension buttonDimension = new Dimension((int) (g.getFontMetrics().stringWidth(this.btnText) * 1.25),
                 (int) (Util.screenHeight * 0.06));
         setPreferredSize(buttonDimension);
     }
@@ -35,15 +34,17 @@ public class RoundButton extends JButton {
     @Override
     protected void paintComponent(Graphics g) {
         setUpDimensions(g);
+
         //button is pressed
         if (getModel().isRollover() | getModel().isPressed()) {
             g.setColor(btnColorDark);
             setForeground(Color.WHITE);
+        //button is not pressed
         } else {
             g.setColor(btnColor);
             setForeground(Color.BLACK);
         }
-        g.fillRoundRect(0, 0, getWidth(), getHeight(), (int) (getWidth()*0.17), (int) (getWidth()*0.17));
+        g.fillRoundRect(0, 0, getWidth(), getHeight(), (int) (getWidth() * 0.17), (int) (getWidth() * 0.17));
 
         super.paintComponent(g);
     }
