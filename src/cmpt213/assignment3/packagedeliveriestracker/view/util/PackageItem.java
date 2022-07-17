9369026db7e1444eb096d37a0f5307515f1469e9
dB (Desktop) <deborah.0214@hotmail.com>
2022-07-16 23:35:17 -0700

    PackageItem sizes correct

--- src/cmpt213/assignment3/packagedeliveriestracker/view/util/PackageItem.java
+++ src/cmpt213/assignment3/packagedeliveriestracker/view/util/PackageItem.java
@@ -5,9 +5,13 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class PackageItem extends JPanel {
-    public PackageItem(){
-        setSize(new Dimension(300,300));
-        setMaximumSize(new Dimension(300,300));
+    private final int width;
+    private final int height;
+    public PackageItem(int width){
+        this.width = width;
+        this.height = width/3;
+        setSize(new Dimension(width,height));
+        setMaximumSize(new Dimension(width,height));
         setBackground(Util.darkBrown);
         this.add(Box.createRigidArea(this.getSize()));
         setBorder(new LineBorder(Util.lightBrown,5));
--- src/cmpt213/assignment3/packagedeliveriestracker/view/util/PackageScrollPane.java
+++ src/cmpt213/assignment3/packagedeliveriestracker/view/util/PackageScrollPane.java
@@ -6,10 +6,8 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class PackageScrollPane extends JScrollPane {
-    private final int width;
-    private final int height;
     private JButton listAllBtn, overdueBtn, upcomingBtn;
-    private JLabel listAllText, overdueText, upcomingText;
+    private  JLabel listAllText, overdueText, upcomingText;
     private final JPanel basePanel;
     private static final JPanel listAllCard = new JPanel();
     private static final JPanel overdueCard = new JPanel();
@@ -18,14 +16,11 @@ public class PackageScrollPane extends JScrollPane {
 
     public PackageScrollPane(int width, int height, ActionListener al) {
 
-        this.width = width;
-        this.height = height;
         this.basePanel = new JPanel(new CardLayout());
         this.columnHeader = new JPanel();
 
         basePanel.setSize(new Dimension((width), (height * 2)));
         basePanel.setMaximumSize(new Dimension(width, (height * 2)));
-        basePanel.setBackground(Color.blue);
         basePanel.setBorder(new LineBorder(Color.BLACK, 5));
 
         setUpViews(al);
@@ -48,39 +43,42 @@ public class PackageScrollPane extends JScrollPane {
 
     private void addCards() {
         basePanel.add(listAllCard);
-        basePanel.add(overdueCard);
-        basePanel.add(upcomingCard);
+//        basePanel.add(upcomingCard);
+//        basePanel.add(overdueCard);
+        upcomingCard.setVisible(false);
+        overdueCard.setVisible(false);
 
-        this.setRowHeaderView(columnHeader);
+//        this.setRowHeaderView(columnHeader);
     }
 
     private void setUpListAll(ActionListener al) {
         this.listAllText = new JLabel("LIST OF ALL PACKAGES");
-        this.listAllCard.setLayout(new BoxLayout(listAllCard, BoxLayout.Y_AXIS));
+        listAllCard.setLayout(new BoxLayout(listAllCard, BoxLayout.Y_AXIS));
         this.listAllBtn = new JButton("LIST ALL");
         this.listAllBtn.setActionCommand("LIST ALL");
         this.listAllBtn.addActionListener(al);
 
-        this.listAllCard.add(new PackageItem(), Box.CENTER_ALIGNMENT);
+        listAllCard.add(new PackageItem(basePanel.getWidth()), Box.CENTER_ALIGNMENT);
     }
 
     private void setUpUpcoming(ActionListener al) {
         this.upcomingText = new JLabel("UPCOMING PACKAGES");
-        this.upcomingCard.setLayout(new BoxLayout(upcomingCard, BoxLayout.Y_AXIS));
+        upcomingCard.setLayout(new BoxLayout(upcomingCard, BoxLayout.Y_AXIS));
         this.upcomingBtn = new JButton("UPCOMING");
         this.upcomingBtn.setActionCommand("UPCOMING");
         this.upcomingBtn.addActionListener(al);
 
-        this.upcomingCard.add(new PackageItem(), Box.CENTER_ALIGNMENT);
+        upcomingCard.add(new PackageItem(basePanel.getWidth()), Box.CENTER_ALIGNMENT);
     }
 
     private void setUpOverdue(ActionListener al) {
         this.overdueText = new JLabel("OVERDUE PACKAGES");
-        this.overdueCard.setLayout(new BoxLayout(overdueCard, BoxLayout.Y_AXIS));
+        overdueCard.setLayout(new BoxLayout(overdueCard, BoxLayout.Y_AXIS));
         this.overdueBtn = new JButton("OVERDUE");
         this.overdueBtn.setActionCommand("OVERDUE");
+        this.overdueBtn.addActionListener(al);
 
-        this.overdueBtn.add(new PackageItem(), Box.CENTER_ALIGNMENT);
+        this.overdueBtn.add(new PackageItem(basePanel.getWidth()), Box.CENTER_ALIGNMENT);
     }
 
     public static void switchView(Util.SCREEN_STATE newState) {
