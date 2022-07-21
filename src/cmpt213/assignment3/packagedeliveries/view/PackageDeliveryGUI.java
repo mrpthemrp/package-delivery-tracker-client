bf1f81995460485f01bfc9007c89aba2427160a2
dB (Desktop) <deborah.0214@hotmail.com>
2022-07-21 07:59:37 -0700

    Write PackageDeliveryGUI JavaDocs

--- src/cmpt213/assignment3/packagedeliveries/view/PackageDeliveryGUI.java
+++ src/cmpt213/assignment3/packagedeliveries/view/PackageDeliveryGUI.java
@@ -1,11 +1,13 @@
 package cmpt213.assignment3.packagedeliveries.view;
 
 import cmpt213.assignment3.packagedeliveries.control.PackageDeliveryControl;
-import cmpt213.assignment3.packagedeliveries.view.util.customUI.AddPackageDialog;
-import cmpt213.assignment3.packagedeliveries.view.util.customUI.ColumnHeader;
-import cmpt213.assignment3.packagedeliveries.view.screens.*;
+import cmpt213.assignment3.packagedeliveries.view.screens.MainScreenLeft;
+import cmpt213.assignment3.packagedeliveries.view.screens.MainScreenRight;
+import cmpt213.assignment3.packagedeliveries.view.screens.StartScreen;
 import cmpt213.assignment3.packagedeliveries.view.util.Util;
 import cmpt213.assignment3.packagedeliveries.view.util.Util.SCREEN_STATE;
+import cmpt213.assignment3.packagedeliveries.view.util.customUI.AddPackageDialog;
+import cmpt213.assignment3.packagedeliveries.view.util.customUI.ColumnHeader;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -13,11 +15,20 @@ import javax.swing.plaf.basic.BasicScrollBarUI;
 import javax.swing.plaf.basic.BasicSplitPaneDivider;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.geom.RoundRectangle2D;
 
 import static java.awt.Scrollbar.VERTICAL;
 
+/**
+ * Main GUI for program. Displays an interactive Package Delivery Tracker.
+ * Inherits {@link JFrame} so that Object itself is a frame that can be invoked on creation.
+ *
+ * @author Deborah Wang
+ */
 public class PackageDeliveryGUI extends JFrame implements ActionListener {
     public static SCREEN_STATE currentState;
     private final JSplitPane mainPanel;
@@ -30,6 +41,11 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
     private final MainScreenRight screenRight;
     private final PackageDeliveryControl packageControl;
 
+    /**
+     * Constructor for GUI.
+     * Initializes final fields and calls helper methods.
+     * StartScreen is added to pane on construction.
+     */
     public PackageDeliveryGUI() {
 
         packageControl = new PackageDeliveryControl();
@@ -73,6 +89,9 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
+    /**
+     * Sets up screen paddings.
+     */
     private void setUpScreenPaddings() {
 
         this.header.setLayout(new FlowLayout());
@@ -91,6 +110,12 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
         this.footer.setBackground(Color.WHITE);
     }
 
+    /**
+     * Sets up main screen contents.
+     *
+     * @link <a href="https://stackoverflow.com/questions/7633354/how-to-hide-the-arrow-buttons-in-a-jscrollbar">...</a> Reference for making up down buttons look invisible
+     * @link <a href="https://stackoverflow.com/questions/8934169/how-to-change-the-color-or-background-color-of-jsplitpane-divider">...</a> Reference for making SplitPane Divider clear
+     */
     private void setUpMainScreen() {
 
         scrollPane.setBackground(Color.WHITE);
@@ -98,13 +123,20 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.getViewport().getView().setBackground(Color.WHITE);
-
-        //refernce for making up down buttons look invisible
-        //https://stackoverflow.com/questions/7633354/how-to-hide-the-arrow-buttons-in-a-jscrollbar
         scrollPane.getVerticalScrollBar().setOrientation(VERTICAL);
         scrollPane.getVerticalScrollBar().setBackground(Color.WHITE);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneLayout.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+
+            @Override
+            protected JButton createDecreaseButton(int orientation) {
+                return createInvisibleButton();
+            }
+
+            @Override
+            protected JButton createIncreaseButton(int orientation) {
+                return createInvisibleButton();
+            }
 
             @Override
             protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
@@ -127,16 +159,6 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
 
             }
 
-            @Override
-            protected JButton createDecreaseButton(int orientation) {
-                return createInvisibleButton();
-            }
-
-            @Override
-            protected JButton createIncreaseButton(int orientation) {
-                return createInvisibleButton();
-            }
-
             private JButton createInvisibleButton() {
                 JButton button = new JButton();
                 Dimension zeroDimension = new Dimension(0, 0);
@@ -155,8 +177,6 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
         this.mainPanel.setDividerLocation((int) (Util.screenWidth * 0.257));
         this.mainPanel.setDividerSize((int) (Util.screenWidth * 0.07));
         this.mainPanel.setEnabled(false);
-        //reference for divider clear:
-        //https://stackoverflow.com/questions/8934169/how-to-change-the-color-or-background-color-of-jsplitpane-divider
         this.mainPanel.setUI(new BasicSplitPaneUI() {
             @Override
             public BasicSplitPaneDivider createDefaultDivider() {
@@ -186,41 +206,21 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
         this.mainPanel.setBorder(BorderFactory.createEmptyBorder());
     }
 
-
-    public void updateStates() {
-
-        switch (currentState) {
-            case LIST_ALL, UPCOMING, OVERDUE -> {
-                columnHeader.buttonClicked(currentState);
-                screenRight.removeAll();
-                screenRight.populateList(currentState);
-                columnHeader.buttonClicked(currentState);
-                scrollPane.setViewportView(screenRight);
-            }
-        }
-        columnHeader.changeColumnText(currentState);
-    }
-
-    private void switchToMain() {
-        this.remove(startPanel);
-        this.add(leftBar, BorderLayout.EAST);
-        this.add(mainPanel, BorderLayout.CENTER);
-        this.setVisible(true);
-
-        this.setTitle("Package Delivery Tracker - Home");
-        currentState = SCREEN_STATE.LIST_ALL;
-    }
-
+    /**
+     * Handles button events that occurs when user presses on them.
+     *
+     * @param e the event to be processed
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("UPDATE")){
+        if (e.getActionCommand().equals("UPDATE")) {
             repaint();
         }
         if (e.getActionCommand().equals("ENTER")) {
             switchToMain();
         }
         if (e.getActionCommand().equals("ADD PACKAGE")) {
-            AddPackageDialog popUp =  new AddPackageDialog(this, "Package Delivery Tracker - Add Package",
+            AddPackageDialog popUp = new AddPackageDialog(this, "Package Delivery Tracker - Add Package",
                     "  C R E A T E  ", "  C A N C E L  ", packageControl);
             popUp.run();
         } else if (e.getActionCommand().equals("LIST ALL")) {
@@ -233,6 +233,36 @@ public class PackageDeliveryGUI extends JFrame implements ActionListener {
         columnHeader.buttonClicked(currentState);
         updateStates();
         repaint();
+    }
+
+    /**
+     * Helper method that switches the frame's content pane to the main panel
+     */
+    private void switchToMain() {
+        this.remove(startPanel);
+        this.add(leftBar, BorderLayout.EAST);
+        this.add(mainPanel, BorderLayout.CENTER);
+        this.setVisible(true);
+
+        this.setTitle("Package Delivery Tracker - Home");
+        currentState = SCREEN_STATE.LIST_ALL;
+    }
+
+    /**
+     * Helper method that updates UI according to screen states.
+     */
+    public void updateStates() {
+
+        switch (currentState) {
+            case LIST_ALL, UPCOMING, OVERDUE -> {
+                columnHeader.buttonClicked(currentState);
+                screenRight.removeAll();
+                screenRight.populateList(currentState);
+                columnHeader.buttonClicked(currentState);
+                scrollPane.setViewportView(screenRight);
+            }
+        }
+        columnHeader.changeColumnText(currentState);
     }
 
 
