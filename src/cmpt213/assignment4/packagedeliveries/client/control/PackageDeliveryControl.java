9f1664e1b9cd044c809992e4c5b0a48947d91a8b
Deborah Wang <deborah.0214@hotmail.com>
2022-08-04 21:53:10 -0700

    Update control

--- src/cmpt213/assignment4/packagedeliveries/client/control/PackageDeliveryControl.java
+++ src/cmpt213/assignment4/packagedeliveries/client/control/PackageDeliveryControl.java
@@ -2,6 +2,9 @@ package cmpt213.assignment4.packagedeliveries.client.control;
 
 import cmpt213.assignment4.packagedeliveries.client.view.util.Util;
 import cmpt213.assignment4.packagedeliveries.client.model.*;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,6 +15,7 @@ import java.util.Collections;
  */
 public class PackageDeliveryControl {
 
+//    private final HttpURLConnection server;
     public final static int DATA_SAVE = 1;
     private final static int DATA_LOAD = 2;
     public final static int REMOVE = 1;
@@ -20,16 +24,14 @@ public class PackageDeliveryControl {
     private static ArrayList<PackageBase> upcomingPackages;
     private static ArrayList<PackageBase> overduePackages;
     private final PackageFactory pkgFactory;
-    private LocalDateTime currentTime;
 
     /**
      * Constructs a PackageDeliveryControl Object.
      * Initializes class fields and also loads in any data from the JSON list.
      */
     public PackageDeliveryControl() {
-
+//        this.server = new
         this.pkgFactory = new PackageFactory();
-        this.currentTime = LocalDateTime.now();
 
         masterListOfPackages = new ArrayList<>();
         upcomingPackages = new ArrayList<>();
@@ -51,7 +53,6 @@ public class PackageDeliveryControl {
                               String extraField, PackageFactory.PackageType type) {
         PackageBase newPackage = pkgFactory.getInstance(type, name, notes, price, weight, date, extraField);
         masterListOfPackages.add(newPackage);
-        updateLists();
     }
 
     /**
@@ -67,71 +68,6 @@ public class PackageDeliveryControl {
         } else if (option == DELIVERY_STATUS) {
             pkg.setDeliveryStatus(newDeliveryStatus);
         }
-        updateLists();
-    }
-
-    /**
-     * Helper method to update package's overdue status.
-     * @param packageDate The date to be compared against the current time.
-     * @return Returns true if package is overdue, false if not.
-     */
-    private boolean isOverdue(LocalDateTime packageDate) {
-        return packageDate.isBefore(currentTime);
-    }
-
-    /**
-     * Helper method to update all lists; lists are also sorted after update.
-     * Current time is updated to LocalDateTime.now() here.
-     */
-    public final void updateLists() {
-        this.currentTime = LocalDateTime.now();
-        //reset upcoming and overdue
-        ArrayList<PackageBase> tempMasterList = new ArrayList<>();
-        upcomingPackages = new ArrayList<>();
-        overduePackages = new ArrayList<>();
-
-        //add necessary packages to lists
-        for (PackageBase tempPkg : masterListOfPackages) {
-            tempMasterList.add(tempPkg);
-            if (!tempPkg.isDelivered()) {
-                if (isOverdue(tempPkg.getExpectedDeliveryDate())) {
-                    if (!overduePackages.contains(tempPkg)) {
-                        overduePackages.add(tempPkg);
-                    }
-                } else {
-                    if (!upcomingPackages.contains(tempPkg)) {
-                        upcomingPackages.add(tempPkg);
-                    }
-                }
-
-            }
-        }
-        masterListOfPackages = tempMasterList;
-
-        //sort
-        Collections.sort(masterListOfPackages);
-        Collections.sort(upcomingPackages);
-        Collections.sort(overduePackages);
-    }
-
-    /**
-     * Helper method that allows the UI to access the lists.
-     * @param currentState Current state of UI tells method which list to return.
-     * @return Returns an ArrayList based on the current state.
-     */
-    public ArrayList<PackageBase> getAListOfPackages(Util.SCREEN_STATE currentState) {
-        switch (currentState) {
-            case LIST_ALL -> {
-                return masterListOfPackages;
-            }
-            case UPCOMING -> {
-                return upcomingPackages;
-            }
-            case OVERDUE -> {
-                return overduePackages;
-            }
-        }
-        return null;
     }
 
     /**
